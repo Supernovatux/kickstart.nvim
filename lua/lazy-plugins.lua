3ac0f43ea6bd0cca954c43640cdcbde37d6f2d85
Thulashitharan <thulashitharan.d@gmail.com>
2025-04-11 03:00:48 +0530

    Added themes

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -35,17 +35,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
-  --
-  -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
-  -- Or use telescope!
-  -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
-  -- you can continue same window with `<space>sr` which resumes last telescope search
+  require 'plugins.better_escape',
 }, {
   ui = {
     icons = vim.g.have_nerd_font and {} or {
--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -2,6 +2,6 @@ return {
   'olimorris/onedarkpro.nvim',
   priority = 1000, -- Ensure it loads first
   config = function()
-    vim.cmd.colorscheme 'onedark_dark'
+    vim.cmd.colorscheme 'onedark'
   end,
 }
